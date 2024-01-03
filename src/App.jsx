89447522ad4cad0c80e9f2fd3c9e97146db43a3a
jamesmed1999 <jamesmed1999@gmail.com>
2024-01-03 15:52:00 -0500

    Finished Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 
 import styles from "./App.module.css"
 import { Navbar } from "./components/Navbar"
+import { Hero } from "./components/hero"
+
 
 function App() {
  
@@ -8,6 +10,7 @@ function App() {
     <>
       <div className = {styles.App}>
       <Navbar />
+      <Hero />
       </div>
     </>
   )
