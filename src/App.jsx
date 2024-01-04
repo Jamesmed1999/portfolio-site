ac5bbe7c2f0432b46294756c6bd4a5f5515832e8
Jamesmed1999 <74335705+Jamesmed1999@users.noreply.github.com>
2024-01-04 15:22:50 -0500

    Finshed about section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import styles from "./App.module.css"
 import { Navbar } from "./components/Navbar"
 import { Hero } from "./components/hero"
+import { About } from "./components/About"
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <div className = {styles.App}>
       <Navbar />
       <Hero />
+      <About />
       </div>
     </>
   )
